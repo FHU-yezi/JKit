0af133b5f3f95442a2b4327a8ad1c5e6f7a06682
FHU-yezi <yehaowei20060411@qq.com>
2021-03-21 05:51:02 +0800

    ref：重构代码结构

---
+++ Basic.py
@@ -0,0 +1,5 @@
+UA = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
+}
+
+parser = "html.parser"
--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import bs4
 import requests
 
-ua = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
-}
+from Errors import *
+from Basic import *
 
 def GetFollowersCount(user_url):
-    html = requests.get(user_url,headers = ua)
-    source = bs4.BeautifulSoup(html.content,"html.parser")
+    html = requests.get(user_url,headers = UA)
+    source = bs4.BeautifulSoup(html.content,parser)
     return int(source.find("div",class_ = "meta-block").p.text)
+
+GetFollowersCount("https://www.jianshu.com/u/ea36c8d8aa30")
