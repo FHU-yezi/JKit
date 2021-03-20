bc621a0a4af1fb25b37008aee3ed1eee9ff5d082
FHU-yezi <yehaowei20060411@qq.com>
2021-03-21 06:05:04 +0800

    feat：实现了查看用户粉丝数、文章数等基本信息

--- main.py
+++ main.py
@@ -7,6 +7,30 @@ from Basic import *
 def GetFollowersCount(user_url):
     html = requests.get(user_url,headers = UA)
     source = bs4.BeautifulSoup(html.content,parser)
-    return int(source.find("div",class_ = "meta-block").p.text)
+    return int(source.findAll("div",class_ = "meta-block")[0].p.text)
 
-GetFollowersCount("https://www.jianshu.com/u/ea36c8d8aa30")
+def GetFansCount(user_url):
+    html = requests.get(user_url,headers = UA)
+    source = bs4.BeautifulSoup(html.content,parser)
+    return int(source.findAll("div",class_ = "meta-block")[1].p.text)
+
+def GetArticlesCount(user_url):
+    html = requests.get(user_url,headers = UA)
+    source = bs4.BeautifulSoup(html.content,parser)
+    return int(source.findAll("div",class_ = "meta-block")[2].p.text)
+
+def GetWordsCount(user_url):
+    html = requests.get(user_url,headers = UA)
+    source = bs4.BeautifulSoup(html.content,parser)
+    return int(source.findAll("div",class_ = "meta-block")[3].p.text)
+
+def GetLikesCount(user_url):
+    html = requests.get(user_url,headers = UA)
+    source = bs4.BeautifulSoup(html.content,parser)
+    return int(source.findAll("div",class_ = "meta-block")[4].p.text)
+
+def GetAssetsCount(user_url):
+    html = requests.get(user_url,headers = UA)
+    source = bs4.BeautifulSoup(html.content,parser)
+    raw_data = source.findAll("div",class_ = "meta-block")[5].p.text
+    return raw_data.replace(".","").replace("w","000")
