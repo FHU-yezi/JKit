54a6837f0c5df75c86409d2492b192967126f0a5
FHU-yezi <yehaowei20060411@qq.com>
2021-03-27 06:53:28 +0800

    fix：修复了找不到手机端 UA 的问题

--- Basic.py
+++ Basic.py
@@ -2,4 +2,8 @@ UA = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'
 }
 
+Mobile_UA = {
+    "User-Agent":"Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/89.0.4389.90"
+}
+
 parser = "html.parser"
