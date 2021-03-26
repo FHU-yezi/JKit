dba533abe1f4d548e330616f856956042c5d203d
FHU-yezi <yehaowei20060411@qq.com>
2021-03-27 06:19:40 +0800

    feat：实现了获取贝壳小岛交易总数

--- main.py
+++ main.py
@@ -78,6 +78,14 @@ def GetBeiKeIslandTotalTradeAmount():
     data = {"ranktype":3,"pageIndex":1}
     raw_data = requests.post("https://www.beikeisland.com/api/Trade/getTradeRankList",headers = headers,json = data)
     raw_data = json.loads(raw_data.content)
-    return int((raw_data["data"]["totalcount"])
+    return int((raw_data["data"]["totalcount"]))
 
-print(GetBeiKeIslandTotalTradeAmount())
+def GetBeiKeIslandTotalTradeCount():
+    headers = {"Host":"www.beikeisland.com",
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36 Edg/89.0.774.57",
+    "Content-Type":"application/json",
+    "Version":"v2.0"}
+    data = {"ranktype":3,"pageIndex":1}
+    raw_data = requests.post("https://www.beikeisland.com/api/Trade/getTradeRankList",headers = headers,json = data)
+    raw_data = json.loads(raw_data.content)
+    return int((raw_data["data"]["totaltime"]))
