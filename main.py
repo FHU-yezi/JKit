4c554257ece1319e7d9d70db1e3d5d4b12a3410e
FHU-yezi <yehaowei20060411@qq.com>
2021-04-01 06:40:37 +0800

    docs：在部分函数中添加了注释

--- main.py
+++ main.py
@@ -7,42 +7,108 @@ import bs4
 import requests
 
 def GetUserName(user_url):
+    """该函数接收一个链接字符串，访问后提取用户昵称。
+
+    Args:
+        user_url (str): 链接字符串，需要加上 https
+
+    Returns:
+        str: 用户昵称
+    """
     html = requests.get(user_url,headers = UA)
     source = bs4.BeautifulSoup(html.content,parser)
     return source.findAll("a",class_ = "name")[0].text
 
 def GetFollowersCount(user_url):
+    """该函数接收一个链接字符串，访问后提取用户的关注人数。
+
+    Args:
+        user_url (str): 链接字符串，需要加上 https
+
+    Returns:
+        int: 用户关注人数
+    """
     html = requests.get(user_url,headers = UA)
     source = bs4.BeautifulSoup(html.content,parser)
     return int(source.findAll("div",class_ = "meta-block")[0].p.text)
 
 def GetFansCount(user_url):
+    """该函数接收一个链接字符串，访问后提取用户的粉丝数。
+
+    Args:
+        user_url (str): 链接字符串，需要加上 https
+
+    Returns:
+        int: 用户粉丝数
+    """
     html = requests.get(user_url,headers = UA)
     source = bs4.BeautifulSoup(html.content,parser)
     return int(source.findAll("div",class_ = "meta-block")[1].p.text)
 
 def GetArticlesCount(user_url):
+    """该函数接收一个链接字符串，访问后提取用户的文章数。
+
+    Args:
+        user_url (str): 链接字符串，需要加上 https
+
+    Returns:
+        int: 用户文章数
+    """
     html = requests.get(user_url,headers = UA)
     source = bs4.BeautifulSoup(html.content,parser)
     return int(source.findAll("div",class_ = "meta-block")[2].p.text)
 
 def GetWordsCount(user_url):
+    """该函数接收一个链接字符串，访问后提取用户的总字数。
+
+    Args:
+        user_url (str): 链接字符串，需要加上 https
+
+    Returns:
+        int: 用户总字数
+    """
     html = requests.get(user_url,headers = UA)
     source = bs4.BeautifulSoup(html.content,parser)
     return int(source.findAll("div",class_ = "meta-block")[3].p.text)
 
 def GetLikesCount(user_url):
+    """该函数接收一个链接字符串，访问后提取用户被喜欢的总数。
+
+    Args:
+        user_url (str): 链接字符串，需要加上 https
+
+    Returns:
+        int: 用户被喜欢数（被点赞数）
+    """
     html = requests.get(user_url,headers = UA)
     source = bs4.BeautifulSoup(html.content,parser)
     return int(source.findAll("div",class_ = "meta-block")[4].p.text)
 
 def GetAssetsCount(user_url):
+    """该函数接收一个链接字符串，访问后提取用户资产量。
+
+    当用户资产大于一定值时，网页中的显示值将以 w 为单位，本函数会对其自动进行处理，但无法突破其精确度限制。
+
+    Args:
+        user_url (str): 链接字符串，需要加上 https
+
+    Returns:
+        int: 用户资产量
+    """
     html = requests.get(user_url,headers = UA)
     source = bs4.BeautifulSoup(html.content,parser)
     raw_data = source.findAll("div",class_ = "meta-block")[5].p.text
     return float(raw_data.replace(".","").replace("w","000"))
 
 def GetUserBasicImformation(user_url):
+    """该函数接收一个链接字符串，访问后提取用户的几项基础信息。
+
+    Args:
+        user_url (str): 链接字符串，需要加上 https
+
+    Returns:
+        str: 用户基础信息
+    """
     html = requests.get(user_url,headers = UA)
     source = bs4.BeautifulSoup(html.content,parser)
     Name = "用户昵称：" + str(source.findAll("a",class_ = "name")[0].text)
@@ -57,6 +123,14 @@ def GetUserBasicImformation(user_url):
     return "".join(Item_List)
 
 def GetBadgesList(user_url):
+    """该函数接收一个链接字符串，访问后提取用户的徽章列表。
+
+    Args:
+        user_url (str): 链接字符串，需要加上 https
+
+    Returns:
+        list: 用户被喜欢数（被点赞数）
+    """
     html = requests.get(user_url,headers = UA)
     source = bs4.BeautifulSoup(html.content,parser)
     Item_List = []
@@ -69,18 +143,36 @@ def GetBadgesList(user_url):
     return Final_List
 
 def GetPersonalIntroduction(user_url):
+    """该函数接收一个链接字符串，访问后提取用户个人简介。
+
+    Args:
+        user_url (str): 链接字符串，需要加上 https
+
+    Returns:
+        int: 用户个人简介
+    """
     html = requests.get(user_url,headers = UA)
     source = bs4.BeautifulSoup(html.content,parser)
     raw_data = str(source.findAll("div",class_ = "js-intro")[0])
     return raw_data.replace('<div class="js-intro">',"").replace("<br/>","\n").replace("</div>","")
 
 def GetBeiKeIslandTotalTradeAmount():
+    """该函数用于获取贝壳小岛的总交易额。
+
+    Returns:
+        int: 总交易额
+    """
     data = {"ranktype":3,"pageIndex":1}
     raw_data = requests.post("https://www.beikeisland.com/api/Trade/getTradeRankList",headers = BeiKeIslandHeaders,json = data)
     raw_data = json.loads(raw_data.content)
     return int((raw_data["data"]["totalcount"]))
 
 def GetBeiKeIslandTotalTradeCount():
+    """该函数用于获取贝壳小岛的总交易次数。
+
+    Returns:
+        int: 总交易次数
+    """
     data = {"ranktype":3,"pageIndex":1}
     raw_data = requests.post("https://www.beikeisland.com/api/Trade/getTradeRankList",headers = BeiKeIslandHeaders,json = data)
     raw_data = json.loads(raw_data.content)
