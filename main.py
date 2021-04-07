8348fe85c411aa0f585de4632f290f185a1cf95f
FHU-yezi <yehaowei20060411@qq.com>
2021-04-08 05:52:20 +0800

    feat：获取用户基础信息改为返回含有信息的字典数据

--- main.py
+++ main.py
@@ -129,20 +129,21 @@ def GetUserBasicImformation(user_url):
         user_url (str): 链接字符串，需要加上 https
 
     Returns:
-        str: 用户基础信息
+        dict: 用户基础信息
     """
     html = requests.get(user_url,headers = UA)
     source = bs4.BeautifulSoup(html.content,parser)
-    Name = "用户昵称：" + str(source.findAll("a",class_ = "name")[0].text)
-    Followers = "关注数：" + str(source.findAll("div",class_ = "meta-block")[0].p.text)
-    Fans = "粉丝数：" + str(source.findAll("div",class_ = "meta-block")[1].p.text)
-    Articles ="文章数：" + str(source.findAll("div",class_ = "meta-block")[2].p.text)
-    Words = "总字数：" + str(source.findAll("div",class_ = "meta-block")[3].p.text)
-    Likes = "喜欢数：" + str(source.findAll("div",class_ = "meta-block")[4].p.text)
+    result = {}
+    result["name"] = source.findAll("a",class_ = "name")[0].text
+    result["followers"] = source.findAll("div",class_ = "meta-block")[0].p.text
+    result["fans"] = source.findAll("div",class_ = "meta-block")[1].p.text
+    result["articles"] = source.findAll("div",class_ = "meta-block")[2].p.text
+    result["words"] = source.findAll("div",class_ = "meta-block")[3].p.text
+    result["likes"] = source.findAll("div",class_ = "meta-block")[4].p.text
     Assets_temp = source.findAll("div",class_ = "meta-block")[5].p.text
+    result["total_assets"] = Assets_temp.replace(".","").replace("w","000")
     Assets = "总资产：" + str(Assets_temp.replace(".","").replace("w","000"))
-    Item_List = [Name,"\n",Followers,"\n",Fans,"\n",Articles,"\n",Words,"\n",Likes,"\n",Assets]
-    return "".join(Item_List)
+    return result
 
 def GetBadgesList(user_url):
     """该函数接收一个链接字符串，访问后提取用户的徽章列表。
