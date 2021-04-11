f063124482f37eacf58237692d645f1b7a9c51ab
FHU-yezi <yehaowei20060411@qq.com>
2021-04-11 22:44:33 +0800

    chore：又大意了，依赖库名称不对

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="JianshuResearchTools",
-    version="0.2.2",
+    version="0.2.3",
     author="FHU-yezi",
     author_email="yehaowei20060411@qq.com",
     description="科技赋能创作星辰",
@@ -10,7 +10,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/FHU-yezi/JianshuResearchTools",
     packages=["JianshuResearchTools"],
-    install_requires=["bs4>=4.9.3","requests>=2.25.1"],
+    install_requires=["beautifulsoup4>=4.9.3","requests>=2.25.1"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
