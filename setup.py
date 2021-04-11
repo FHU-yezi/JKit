aa324f8a569853fa4681b70e40cbd867db9f61bb
FHU-yezi <yehaowei20060411@qq.com>
2021-04-11 15:42:04 +0800

    chore：添加了 setup.py 文件

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="JianshuResearchTools",
+    version="0.2.1",
+    author="FHU-yezi",
+    author_email="yehaowei20060411@qq.com",
+    description="科技赋能创作星辰",
+    long_description="简书数据获取与分析库",
+    long_description_content_type="text/markdown",
+    url="https://github.com/FHU-yezi/JianshuResearchTools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
