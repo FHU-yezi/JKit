73e746767b493f8ec199b81254291fd89c005674
FHU-yezi <yehaowei20060411@qq.com>
2021-04-21 21:59:54 +0800

    chore：为发布 0.3.0 版本做好准备

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 import setuptools
 
+with open("README.md","r",encoding = "utf-8") as file:
+    long_description = file.read()
+
 setuptools.setup(
     name="JianshuResearchTools",
-    version="0.2.4",
+    version="0.3.0",
     author="FHU-yezi",
     author_email="yehaowei20060411@qq.com",
     description="科技赋能创作星辰",
-    long_description="简书数据获取与分析库",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/FHU-yezi/JianshuResearchTools",
     packages=["JianshuResearchTools"],
