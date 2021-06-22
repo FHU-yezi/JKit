0eb067a364ecd9d7c91bb171351b1937195e7b33
FHU-yezi <yehaowei20060411@qq.com>
2021-06-22 15:01:23 +0800

    feat：增加 headers

---
+++ JianshuResearchTools/basic.py
@@ -0,0 +1,16 @@
+PC_header = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"
+}
+
+mobile_header = {
+    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/89.0.4389.90"
+}
+
+jianshu_request_header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36 Edg/89.0.774.57",
+                          "X-INFINITESCROLL": "true",
+                          "X-Requested-With": "XMLHttpRequest"}
+
+beikeisland_request_header = {"Host": "www.beikeisland.com",
+                            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36 Edg/89.0.774.57",
+                            "Content-Type": "application/json",
+                            "Version": "v2.0"}
