9ed8d58cdd3b71068bdd1abff9e6a662df89debd
FHU-yezi <yehaowei20060411@qq.com>
2021-06-23 14:14:09 +0800

    feat：实现获取贝壳小岛三种交易排行榜的数据

--- JianshuResearchTools/beikeisland.py
+++ JianshuResearchTools/beikeisland.py
@@ -2,10 +2,11 @@ import json
 
 import requests
 
-from basic import beikeisland_request_header
+from basic import BeikeIsland_request_header
+from convert import UserUrlToUserSlug
 
 
-def GetBeiKeIslandTotalTradeAmount() -> int:
+def GetBeikeIslandTotalTradeAmount() -> int:
     # TODO: 注释优化
     """该函数返回贝壳小岛的总交易额。
 
@@ -14,12 +15,12 @@ def GetBeiKeIslandTotalTradeAmount() -> int:
     """
     data = {}  # 不传送数据也能正常获取，节省时间和带宽
     source = requests.post("https://www.beikeisland.com/api/Trade/getTradeRankList", 
-                            headers=beikeisland_request_header, json=data)
+                            headers=BeikeIsland_request_header, json=data)
     json_obj = json.loads(source.content)
     result = json_obj["data"]["totalcount"]
     return result
 
-def GetBeiKeIslandTotalTradeCount() -> int:
+def GetBeikeIslandTotalTradeCount() -> int:
     # TODO: 注释优化
     """该函数返回贝壳小岛的总交易次数。
 
@@ -28,7 +29,97 @@ def GetBeiKeIslandTotalTradeCount() -> int:
     """
     data = {}  # 不传送数据也能正常获取，节省时间和带宽
     source = requests.post("https://www.beikeisland.com/api/Trade/getTradeRankList", 
-                            headers=beikeisland_request_header, json=data)
+                            headers=BeikeIsland_request_header, json=data)
     json_obj = json.loads(source.content)
     result = json_obj["data"]["totaltime"]
+    return result
+
+def GetBeikeIslandTotalTradeRankInfo(page: int =1) -> list:
+    """该函数接收一个页码参数，并返回贝壳小岛总交易排行榜中对应页码的用户信息
+
+    Args:
+        page (int, optional): 排行榜页码. Defaults to 1.
+
+    Returns:
+        list: 总交易排行榜中对应页码的用户信息
+    """
+    data = {
+        "ranktype": 3, 
+        "pageIndex": page
+    }
+    source = requests.post("https://www.beikeisland.com/api/Trade/getTradeRankList", 
+                            headers=BeikeIsland_request_header, json=data)
+    json_obj = json.loads(source.content)
+    result = []
+    for item in json_obj["data"]["ranklist"]:
+        item_info = {
+            "bkuid": item["userid"], 
+            "jianshuname": item["jianshuname"], 
+            "avatar": item["avatarurl"], 
+            "userurl": item["jianshupath"], 
+            "uslug": UserUrlToUserSlug(item["jianshupath"]), 
+            "total_trade_amount": item["totalamount"], 
+            "total_trade_times": item["totaltime"]
+        }
+        result.append(item_info)
+    return result
+
+def GetBeikeIslandBuyTradeRankInfo(page: int =1) -> list:
+    """该函数接收一个页码参数，并返回贝壳小岛买贝排行榜中对应页码的用户信息
+
+    Args:
+        page (int, optional): 排行榜页码. Defaults to 1.
+
+    Returns:
+        list: 买贝排行榜中对应页码的用户信息
+    """
+    data = {
+        "ranktype": 1, 
+        "pageIndex": page
+    }
+    source = requests.post("https://www.beikeisland.com/api/Trade/getTradeRankList", 
+                            headers=BeikeIsland_request_header, json=data)
+    json_obj = json.loads(source.content)
+    result = []
+    for item in json_obj["data"]["ranklist"]:
+        item_info = {
+            "bkuid": item["userid"], 
+            "jianshuname": item["jianshuname"], 
+            "avatar": item["avatarurl"], 
+            "userurl": item["jianshupath"], 
+            "uslug": UserUrlToUserSlug(item["jianshupath"]), 
+            "total_trade_amount": item["totalamount"], 
+            "total_trade_times": item["totaltime"]
+        }
+        result.append(item_info)
+    return result
+
+def GetBeikeIslandSellTradeRankInfo(page: int =1) -> list:
+    """该函数接收一个页码参数，并返回贝壳小岛卖贝排行榜中对应页码的用户信息
+
+    Args:
+        page (int, optional): 排行榜页码. Defaults to 1.
+
+    Returns:
+        list: 卖贝排行榜中对应页码的用户信息
+    """
+    data = {
+        "ranktype": 2, 
+        "pageIndex": page
+    }
+    source = requests.post("https://www.beikeisland.com/api/Trade/getTradeRankList", 
+                            headers=BeikeIsland_request_header, json=data)
+    json_obj = json.loads(source.content)
+    result = []
+    for item in json_obj["data"]["ranklist"]:
+        item_info = {
+            "bkuid": item["userid"], 
+            "jianshuname": item["jianshuname"], 
+            "avatar": item["avatarurl"], 
+            "userurl": item["jianshupath"], 
+            "uslug": UserUrlToUserSlug(item["jianshupath"]), 
+            "total_trade_amount": item["totalamount"], 
+            "total_trade_times": item["totaltime"]
+        }
+        result.append(item_info)
     return result
