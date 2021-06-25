57410afee727f89700f56a5eb476e49796d40275
FHU-yezi <yehaowei20060411@qq.com>
2021-06-25 10:09:42 +0800

    feat：支持判断是否是简书 Url

--- JianshuResearchTools/assert_funcs.py
+++ JianshuResearchTools/assert_funcs.py
@@ -3,6 +3,20 @@ from basic import jianshu_request_header
 import json
 import requests
 
+def AssertJianshuUrl(string: str) -> None:
+    """该函数接收一个字符串，并在其不是有效的简书 Url 时抛出异常
+
+    Args:
+        string (str): 需要进行判断的字符串
+
+    Raises:
+        InputError: 意味着传入的参数不是有效的简书 Url
+    """
+    keyword_to_find = ["https://", "www.jianshu.com"]
+    for keyword in keyword_to_find:
+        if string.find(keyword) == -1:
+            raise InputError("参数" + string + "不是有效的简书 Url")
+
 
 def AssertUserUrl(string: str) -> None:
     """该函数接收一个字符串，并在其不是有效的简书用户主页 Url 时抛出异常
