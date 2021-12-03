d268039af53f49834c06fb8036ba1d782007fc59
FHU-yezi <yehaowei20060411@qq.com>
2021-12-03 22:53:07 +0800

    chore：更新版本号和依赖信息

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/FHU-yezi/JianshuResearchTools",
     packages=["JianshuResearchTools"],
-    install_requires=["lxml>=4.6.3","requests>=2.25.1"],
+    install_requires=["lxml==4.6.3","httpx==0.19.0"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
