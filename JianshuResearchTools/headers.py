2c1c6b75923451874932a5e68f5ffa6888521236
FHU-yezi <yehaowei20060411@qq.com>
2022-05-30 16:12:52 +0800

    style: 代码格式优化

--- JianshuResearchTools/headers.py
+++ JianshuResearchTools/headers.py
@@ -1,19 +1,25 @@
-__all__ = ["PC_header", "jianshu_request_header", "BeikeIsland_request_header", "mobile_header"]
-
+__all__ = [
+    "PC_header", "api_request_header", "BeikeIsland_request_header",
+    "mobile_header"
+]
 
 PC_header = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36"
 }
 
-jianshu_request_header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36 Edg/89.0.774.57",
-                          "X-INFINITESCROLL": "true",
-                          "X-Requested-With": "XMLHttpRequest"
-                          }
+api_request_header = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36 Edg/89.0.774.57",
+    "X-INFINITESCROLL": "true",
+    "X-Requested-With": "XMLHttpRequest"
+}
 
-BeikeIsland_request_header = {"Host": "www.beikeisland.com",
-                              "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36 Edg/89.0.774.57",
-                              "Content-Type": "application/json",
-                              "Version": "v2.0"
-                              }
+BeikeIsland_request_header = {
+    "Host": "www.beikeisland.com",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36 Edg/89.0.774.57",
+    "Content-Type": "application/json",
+    "Version": "v2.0"
+}
 
-mobile_header = {"User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.134 Mobile Safari/537.36"}
+mobile_header = {
+    "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.134 Mobile Safari/537.36"
+}
