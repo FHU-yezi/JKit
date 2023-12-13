dc0b240da68420f83045fafad4acc3e7b5e6f620
yezi <yehaowei20060411@qq.com>
2023-12-13 17:31:08 +0800

    feat: 添加 `get_json` 函数

--- jkit/_http_client.py
+++ jkit/_http_client.py
@@ -1,3 +1,19 @@
+from typing import Any, Dict, Optional
+
+from msgspec.json import Decoder
+
 from jkit.config import NETWORK_CONFIG
 
 HTTP_CLIENT = NETWORK_CONFIG._get_http_client()
+JSON_DECODER = Decoder()
+
+
+async def get_json(
+    endpoint: str,
+    path: str,
+    params: Optional[Dict[str, Any]] = None,
+) -> Dict[str, Any]:
+    response = await HTTP_CLIENT.get(f"{endpoint}{path}", params=params)
+    response.raise_for_status()
+
+    return JSON_DECODER.decode(response.content)
