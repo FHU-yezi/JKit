29807cdbb7ba8ebea67ab05a7f3b93e82c87b556
yezi <yehaowei20060411@qq.com>
2023-12-16 09:04:38 +0800

    feat: 优化 Struct 输出格式

--- jkit/_base.py
+++ jkit/_base.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from msgspec import Struct, convert, to_builtins
 from typing_extensions import Self
 
@@ -5,6 +7,22 @@ from typing_extensions import Self
 class DataObject(Struct):
     def _validate(self) -> Self:
         return convert(to_builtins(self), type=self.__class__)
+
+    def __repr__(self) -> str:
+        field_strings: List[str] = []
+        for field in self.__struct_fields__:
+            value = self.__getattribute__(field)
+
+            if isinstance(value, str) and len(value) >= 100:
+                formatted_value = value[:100] + "[truncated...]"
+            else:
+                formatted_value = value.__repr__()
+
+            field_strings.append(f"{field}={formatted_value}")
+
+        return (
+            self.__class__.__name__ + "(\n    " + ",\n    ".join(field_strings) + "\n)"
+        )
 
 
 DATA_OBJECT_CONFIG = {
