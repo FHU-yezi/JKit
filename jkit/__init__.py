4a944370dfa605d383672c18c59c89091dc0acf5
yezi <yehaowei20060411@qq.com>
2023-12-19 17:48:03 +0800

    feat: 移除主包导出

--- jkit/__init__.py
+++ jkit/__init__.py
@@ -1,8 +1,1 @@
-from jkit.article import Article
-from jkit.config import ENDPOINT_CONFIG, NETWORK_CONFIG
-from jkit.exceptions import APIUnsupportedError, JKitError
-from jkit.ranking.article_earning import ArticleEarningRank
-from jkit.ranking.assets import AssetsRank
-from jkit.user import User
-
-VERSION = "3.0.0a2"
+VERSION = "3.0.0a3"
