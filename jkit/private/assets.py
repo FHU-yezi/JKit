d1f3a015ff9a213ad1ee1a233c031964ae4986b3
yezi <yehaowei20060411@qq.com>
2024-01-01 13:19:41 +0800

    feat: 修改私有资产接口实现

--- jkit/private/assets.py
+++ jkit/private/assets.py
@@ -1,5 +1,5 @@
 from decimal import Context, Decimal
-from typing import AsyncGenerator, Literal, Optional
+from typing import AsyncGenerator, Optional
 
 from jkit._base import DATA_OBJECT_CONFIG, DataObject, ResourceObject
 from jkit._constraints import (
@@ -23,13 +23,10 @@ class AssetsTransactionRecord(DataObject, **DATA_OBJECT_CONFIG):
 
 
 class AssetsTransactionHistory(ResourceObject):
-    def __init__(
-        self, *, credential: JianshuCredential, assets_type: Literal["FP", "FTN"]
-    ) -> None:
+    def __init__(self, *, credential: JianshuCredential) -> None:
         self._credential = credential
-        self._assets_type = assets_type
 
-    async def iter_records(
+    async def iter_fp_records(
         self, *, max_id: Optional[int] = None
     ) -> AsyncGenerator[AssetsTransactionRecord, None]:
         now_max_id = max_id
@@ -37,10 +34,40 @@ class AssetsTransactionHistory(ResourceObject):
         while True:
             data = await get_json(
                 endpoint=ENDPOINT_CONFIG.jianshu,
-                path={
-                    "FP": "/asimov/fp_wallets/transactions",
-                    "FTN": "/asimov/fp_wallets/jsb_transactions",
-                }[self._assets_type],
+                path="/asimov/fp_wallets/transactions",
+                params={"since_id": 0, "max_id": now_max_id}
+                if now_max_id
+                else {"since_id": 0},
+                cookies=self._credential.cookies,
+            )
+            if not data["transactions"]:
+                return
+
+            now_max_id = data["transactions"][-1]["id"]
+
+            for item in data["transactions"]:
+                is_out = item["io_type"] == 2
+                yield AssetsTransactionRecord(
+                    id=item["id"],
+                    time=normalize_datetime(item["time"]),
+                    type_id=item["tn_type"],
+                    type_text=item["display_name"],
+                    amount=normalize_assets_amount(item["amount"])
+                    * (-1 if is_out else 1),
+                    amount_precise=Context(prec=18).create_decimal_from_float(
+                        (item["amount_18"] * (-1 if is_out else 1)) / 10**18
+                    ),
+                )._validate()
+
+    async def iter_ftn_records(
+        self, *, max_id: Optional[int] = None
+    ) -> AsyncGenerator[AssetsTransactionRecord, None]:
+        now_max_id = max_id
+
+        while True:
+            data = await get_json(
+                endpoint=ENDPOINT_CONFIG.jianshu,
+                path="/asimov/fp_wallets/jsb_transactions",
                 params={"since_id": 0, "max_id": now_max_id}
                 if now_max_id
                 else {"since_id": 0},
