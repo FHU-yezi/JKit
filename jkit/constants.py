40393428e5ba779f4428a40f46edc18108f55ee6
yezi <yehaowei20060411@qq.com>
2024-02-18 23:09:20 +0800

    feat: 支持全部 Slug 包含大写字母，其行为与小写字母一致

--- jkit/constants.py
+++ jkit/constants.py
@@ -2,19 +2,19 @@ from re import compile as regex_compile
 
 USER_NAME_REGEX = regex_compile(r"^[\w]*$")
 
-JIANSHU_URL_REGEX = regex_compile(r"^https://www\.jianshu\.com/[a-z0-9/]*/?$")
+JIANSHU_URL_REGEX = regex_compile(r"^https://www\.jianshu\.com/[a-zA-Z0-9/]*/?$")
 USER_UPLOADED_URL_REGEX = regex_compile(r"^https?:\/\/.*\.jianshu\.io\/[\w%-\/]*\/?$")
 USER_URL_REGEX = regex_compile(r"^https://www\.jianshu\.com/u/[a-zA-Z0-9]{6,12}/?$")
-ARTICLE_URL_REGEX = regex_compile(r"^https://www\.jianshu\.com/p/[a-z0-9]{12}/?$")
+ARTICLE_URL_REGEX = regex_compile(r"^https://www\.jianshu\.com/p/[a-zA-Z0-9]{12}/?$")
 NOTEBOOK_URL_REGEX = regex_compile(r"^https://www\.jianshu\.com/nb/\d{7,8}/?$")
-COLLECTION_URL_REGEX = regex_compile(r"^https://www\.jianshu\.com/c/[a-z0-9]{6,12}/?$")
-ISLAND_URL_REGEX = regex_compile(r"^https://www\.jianshu\.com/g/[a-z0-9]{16}/?$")
+COLLECTION_URL_REGEX = regex_compile(r"^https://www\.jianshu\.com/c/[a-zA-Z0-9]{6,12}/?$")
+ISLAND_URL_REGEX = regex_compile(r"^https://www\.jianshu\.com/g/[a-zA-Z0-9]{16}/?$")
 
 USER_SLUG_REGEX = regex_compile(r"^[a-zA-Z0-9]{6,12}$")
-ARTICLE_SLUG_REGEX = regex_compile(r"^[a-z0-9]{12}$")
+ARTICLE_SLUG_REGEX = regex_compile(r"^[a-zA-Z0-9]{12}$")
 NOTEBOOK_ID_MIN = 10000000
 NOTEBOOK_ID_MAX = 99999999
-COLLECTION_SLUG_REGEX = regex_compile(r"^[a-z0-9]{6,12}$")
-ISLAND_SLUG_REGEX = regex_compile(r"^[a-z0-9]{16}$")
+COLLECTION_SLUG_REGEX = regex_compile(r"^[a-zA-Z0-9]{6,12}$")
+ISLAND_SLUG_REGEX = regex_compile(r"^[a-zA-Z0-9]{16}$")
 
 MAX_ID = 10**9
