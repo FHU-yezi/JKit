7f581ebb9c864124cc7cc632455734427081d5bc
yezi <yehaowei20060411@qq.com>
2024-02-18 15:30:21 +0800

    fix: 不允许 Slug 末尾存在 / 符号

--- jkit/constants.py
+++ jkit/constants.py
@@ -10,11 +10,11 @@ NOTEBOOK_URL_REGEX = regex_compile(r"^https://www\.jianshu\.com/nb/\d{7,8}/?$")
 COLLECTION_URL_REGEX = regex_compile(r"^https://www\.jianshu\.com/c/[a-z0-9]{6,12}/?$")
 ISLAND_URL_REGEX = regex_compile(r"^https://www\.jianshu\.com/g/[a-z0-9]{16}/?$")
 
-USER_SLUG_REGEX = regex_compile(r"^[a-z0-9]{6,12}/?$")
-ARTICLE_SLUG_REGEX = regex_compile(r"^[a-z0-9]{12}/?$")
+USER_SLUG_REGEX = regex_compile(r"^[a-z0-9]{6,12}$")
+ARTICLE_SLUG_REGEX = regex_compile(r"^[a-z0-9]{12}$")
 NOTEBOOK_ID_MIN = 10000000
 NOTEBOOK_ID_MAX = 99999999
-COLLECTION_SLUG_REGEX = regex_compile(r"^[a-z0-9]{6,12}/?$")
-ISLAND_SLUG_REGEX = regex_compile(r"^[a-z0-9]{16}/?$")
+COLLECTION_SLUG_REGEX = regex_compile(r"^[a-z0-9]{6,12}$")
+ISLAND_SLUG_REGEX = regex_compile(r"^[a-z0-9]{16}$")
 
 MAX_ID = 10**9
