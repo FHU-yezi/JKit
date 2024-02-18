121f5db405d870e65c440fa077bb23ccbe5d05fb
yezi <yehaowei20060411@qq.com>
2024-02-18 23:06:53 +0800

    fix: 修复用户 Slug 中包含大写字母时校验失败的问题

--- jkit/constants.py
+++ jkit/constants.py
@@ -4,13 +4,13 @@ USER_NAME_REGEX = regex_compile(r"^[\w]*$")
 
 JIANSHU_URL_REGEX = regex_compile(r"^https://www\.jianshu\.com/[a-z0-9/]*/?$")
 USER_UPLOADED_URL_REGEX = regex_compile(r"^https?:\/\/.*\.jianshu\.io\/[\w%-\/]*\/?$")
-USER_URL_REGEX = regex_compile(r"^https://www\.jianshu\.com/u/[a-z0-9]{6,12}/?$")
+USER_URL_REGEX = regex_compile(r"^https://www\.jianshu\.com/u/[a-zA-Z0-9]{6,12}/?$")
 ARTICLE_URL_REGEX = regex_compile(r"^https://www\.jianshu\.com/p/[a-z0-9]{12}/?$")
 NOTEBOOK_URL_REGEX = regex_compile(r"^https://www\.jianshu\.com/nb/\d{7,8}/?$")
 COLLECTION_URL_REGEX = regex_compile(r"^https://www\.jianshu\.com/c/[a-z0-9]{6,12}/?$")
 ISLAND_URL_REGEX = regex_compile(r"^https://www\.jianshu\.com/g/[a-z0-9]{16}/?$")
 
-USER_SLUG_REGEX = regex_compile(r"^[a-z0-9]{6,12}$")
+USER_SLUG_REGEX = regex_compile(r"^[a-zA-Z0-9]{6,12}$")
 ARTICLE_SLUG_REGEX = regex_compile(r"^[a-z0-9]{12}$")
 NOTEBOOK_ID_MIN = 10000000
 NOTEBOOK_ID_MAX = 99999999
