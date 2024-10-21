f6e46679a6a7dc44c17c0f8e4e08f5954e2b7632
FHU-yezi <yehaowei20060411@qq.com>
2024-10-21 22:10:00 +0800

    fix: 修复标识符检测与转换对部分旧版本 slug 误判为无效标识符的问题

--- jkit/constants.py
+++ jkit/constants.py
@@ -7,20 +7,20 @@ BLANK_LINES_REGEX = regex_compile("\n{2,}")
 
 JIANSHU_URL_REGEX = regex_compile(r"^https://www\.jianshu\.com/[a-zA-Z0-9/]*/?$")
 USER_UPLOADED_URL_REGEX = regex_compile(r"^https?:\/\/.*/?$")
-USER_URL_REGEX = regex_compile(r"^https://www\.jianshu\.com/u/[a-zA-Z0-9]{6,12}/?$")
-ARTICLE_URL_REGEX = regex_compile(r"^https://www\.jianshu\.com/p/[a-zA-Z0-9]{12}/?$")
-NOTEBOOK_URL_REGEX = regex_compile(r"^https://www\.jianshu\.com/nb/\d{7,8}/?$")
+USER_URL_REGEX = regex_compile(r"^https://www\.jianshu\.com/u/([a-z0-9]{12}|[a-zA-z0-9]{6})/?$")
+ARTICLE_URL_REGEX = regex_compile(r"^https://www\.jianshu\.com/p/([a-z0-9]{12}|[a-zA-z0-9]{6})/?$")
+NOTEBOOK_URL_REGEX = regex_compile(r"^https://www\.jianshu\.com/nb/\d{6,8}/?$")
 COLLECTION_URL_REGEX = regex_compile(
     r"^https://www\.jianshu\.com/c/[a-zA-Z0-9]{6,12}/?$"
 )
 ISLAND_URL_REGEX = regex_compile(r"^https://www\.jianshu\.com/g/[a-zA-Z0-9]{16}/?$")
 
-USER_SLUG_REGEX = regex_compile(r"^[a-zA-Z0-9]{6,12}$")
-ARTICLE_SLUG_REGEX = regex_compile(r"^[a-zA-Z0-9]{12}$")
-COLLECTION_SLUG_REGEX = regex_compile(r"^[a-zA-Z0-9]{6,12}$")
+USER_SLUG_REGEX = regex_compile(r"^[a-z0-9]{12}$|^[a-zA-z0-9]{6}$")
+ARTICLE_SLUG_REGEX = regex_compile(r"^[a-z0-9]{12}$|^[a-zA-z0-9]{6}$")
+COLLECTION_SLUG_REGEX = regex_compile(r"^[a-z0-9]{12}$|^[a-zA-z0-9]{6}$")
 ISLAND_SLUG_REGEX = regex_compile(r"^[a-zA-Z0-9]{16}$")
 
-NOTEBOOK_ID_MIN = 10000000
+NOTEBOOK_ID_MIN = 100000
 NOTEBOOK_ID_MAX = 99999999
 
-USER_NAME_REGEX = regex_compile(r"^[\w]*$")
+USER_NAME_REGEX = regex_compile(r"^[\w]{,15}$")
